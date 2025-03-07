9dd2f0b8114d3478f2f66cfc87f9fa1d0c9a3091
Kirirll Spiridonov <vo-ro@list.ru>
2025-03-07 13:03:03 +0300

    got rid of the raw pointers

--- include/rbtree.hpp
+++ include/rbtree.hpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <iostream>
+#include <memory>
 #include <queue>
 #include <stack>
 
@@ -11,26 +12,27 @@ enum class COLOR {
 
 template<typename T>
 struct Node {
-	Node *left_ = nullptr, *right_ = nullptr, *parent_ = nullptr;
+  std::shared_ptr<Node<T>> left_, right_, parent_;
 	COLOR color_;      // color of node
 	size_t nodes_ = 1; // holds number of nodes that contains left subtree +
 					   // right subtree + this node
 	T key_;      // stored value
-	Node(T key, COLOR color = Trees::COLOR::RED) : key_(key), color_(color) { }
+  Node() = default;
+	Node(T key, Trees::COLOR color = Trees::COLOR::RED) : key_(key), color_(color) { }
 };
 
-template<typename T>
+template<typename T=int>
 struct Iter {
-	const Node<T> *node_;
+  std::shared_ptr<const Node<T>> node_;
 	int is_less_ = 0;  // stores number of elements that are less current
   Iter() = default;
-	Iter(Node<T> *node, size_t is_less) : node_(node), is_less_(is_less) {}
+	Iter(std::shared_ptr<Node<T>> node, size_t is_less) : node_(node), is_less_(is_less) {}
 };
 
 template<typename KeyT=int, typename Comp=std::less<KeyT>>
 class RBTree final {
-	Node<KeyT> *root_ = nullptr;
-	using iterator = const Node<KeyT> *;
+  Comp cmp = Comp{};
+  std::shared_ptr<Node<KeyT>> root_{};
 	size_t sz_ = 0;
 public:
   size_t size() const {
@@ -40,18 +42,18 @@ public:
   // Returns an Iter<T> pointing to an element greater than or equal to the key
   // If there is no such element Iter<T>.node == nullptr
 	Iter<KeyT> lower_bound(KeyT key) const { 
-		Node<KeyT> *cur_node = root_;
-		Node<KeyT> *res_node = nullptr;
+    std::shared_ptr<Node<KeyT>> cur_node = root_;
+		std::shared_ptr<Node<KeyT>> res_node = nullptr;
 		int is_less = sz_;
 		while (cur_node != nullptr) {
-			if (key > cur_node->key_) {
+			if (cmp(cur_node->key_, key)) {
 				cur_node = cur_node->right_;
 				continue;
 			}
 			is_less -= ((cur_node->right_ ? cur_node->right_->nodes_ : 0) + 1);
 			if (res_node == nullptr)
 				res_node = cur_node;
-			else if (res_node->key_ > cur_node->key_) {
+			else if (cmp(cur_node->key_, res_node->key_)) {
 				res_node = cur_node;
 			}
 			cur_node = cur_node->left_;
@@ -64,18 +66,18 @@ public:
   // Returns an Iter<T> pointing to an element greater than the key
   // If there is no such element Iter<T>.node == nullptr
 	Iter<KeyT> upper_bound(KeyT key) const {
-		Node<KeyT> *cur_node = root_;
-		Node<KeyT> *res_node = nullptr;
+    std::shared_ptr<Node<KeyT>> cur_node = root_;
+    std::shared_ptr<Node<KeyT>> res_node = nullptr;
 		int is_less = sz_;
 		while (cur_node != nullptr) {
-			if (key >= cur_node->key_) {
+			if (!cmp(key, cur_node->key_)) {
 				cur_node = cur_node->right_;
 				continue;
 			}
 			is_less -= ((cur_node->right_ ? cur_node->right_->nodes_ : 0) + 1);
 			if (res_node == nullptr)
 				res_node = cur_node;
-			else if (res_node->key_ > cur_node->key_) {
+			else if (cmp(cur_node->key_, res_node->key_)) {
 				res_node = cur_node;
 			}
 			cur_node = cur_node->left_;
@@ -87,24 +89,24 @@ public:
   // Insert key to the Red-Black Tree
 	void insert(KeyT key) {
 		if (sz_ == 0) {
-			root_ = new Node(key, Trees::COLOR::BLACK);
+			root_ = std::make_shared<Node<KeyT>>(key, Trees::COLOR::BLACK);
 			++sz_;
 			return;
 		}
-		Node<KeyT> *cur_node = root_;	
-		Node<KeyT> *prev_node;	
+	  std::shared_ptr<Node<KeyT>> cur_node = root_;	
+		std::shared_ptr<Node<KeyT>> prev_node;	
 		while (cur_node != nullptr) {
 			prev_node = cur_node;
-			if (key == cur_node->key_)
+			if (!cmp(key, cur_node->key_) and !cmp(cur_node->key_, key))
 				return;
-			if (key < cur_node->key_)
+			if (cmp(key, cur_node->key_))
 				cur_node = cur_node->left_;
 			else 
 				cur_node = cur_node->right_;
 		}
-		cur_node = new Node(key);
+		cur_node = std::make_shared<Node<KeyT>>(key);
 		++sz_;
-		if (key < prev_node->key_)
+		if (cmp(key, prev_node->key_))
 			prev_node->left_ = cur_node;
 		else 
 			prev_node->right_ = cur_node;
@@ -130,44 +132,35 @@ public:
   ~RBTree() {
     if (root_ == nullptr) 
       return;
-    std::stack<Trees::Node<KeyT> *> q;
+    std::stack<std::shared_ptr<Node<KeyT>>> q;
     q.push(root_);
     while (!q.empty()) {
-      Node<KeyT> *cur_node = q.top();
-      if (cur_node->left_ == nullptr and cur_node->right_ == nullptr) {
-        if (cur_node->parent_ and cur_node->parent_->left_ == cur_node) {
-          cur_node->parent_->left_ = nullptr;
-        }
-        if (cur_node->parent_ and cur_node->parent_->right_ == cur_node) {
-          cur_node->parent_->right_ = nullptr;
-        }
-        delete cur_node;
-        q.pop();
-        continue;
-      }
+      std::shared_ptr<Node<KeyT>> cur_node = q.top();
+      q.pop();
       if (cur_node->left_ != nullptr) {
         q.push(cur_node->left_);
-        continue;
+        cur_node->left_ = nullptr;
       }
       if (cur_node->right_ != nullptr) {
         q.push(cur_node->right_);
+        cur_node->right_ = nullptr;
       }
     }
   }
 
 private:
 
-	void recount_nodes(Node<KeyT> *cur_node) {
+	void recount_nodes(std::shared_ptr<Node<KeyT>> cur_node) {
 		while (cur_node != nullptr) {
 			++cur_node->nodes_;
 			cur_node = cur_node->parent_;
 		}
 	}
   
-	void balance_tree(Node<KeyT> *cur_node) {
+	void balance_tree(std::shared_ptr<Node<KeyT>> cur_node) {
 		while (cur_node->parent_ != nullptr and cur_node->parent_->color_ == Trees::COLOR::RED) {
 			if (cur_node->parent_ == cur_node->parent_->parent_->left_) {
-				Node<KeyT> *uncle = cur_node->parent_->parent_->right_;
+        std::shared_ptr<Node<KeyT>> uncle = cur_node->parent_->parent_->right_;
 				if (uncle == nullptr or uncle->color_ == Trees::COLOR::BLACK) {
 					if (cur_node == cur_node->parent_->right_) { // i.e. cur_node is right child
 						cur_node = cur_node->parent_;
@@ -185,7 +178,7 @@ private:
 				}
 			}
 			else {
-				Node<KeyT> *uncle = cur_node->parent_->parent_->left_;
+        std::shared_ptr<Node<KeyT>> uncle = cur_node->parent_->parent_->left_;
 				if (uncle == nullptr or uncle->color_ == Trees::COLOR::BLACK) {
 					if (cur_node == cur_node->parent_->left_) { // i.e. cur_node is left child
 						cur_node = cur_node->parent_;
@@ -214,8 +207,8 @@ private:
 	 *    [a]  [b]                [b]  [g] 
 	 *   x = cur_node
 	*/
-	void right_rotate(Node<KeyT> *cur_node) {
-		Node<KeyT> *y = cur_node->left_;
+	void right_rotate(std::shared_ptr<Node<KeyT>> cur_node) {
+    std::shared_ptr<Node<KeyT>> y = cur_node->left_;
 		cur_node->left_ = y->right_;
 		if (y->right_ != nullptr)
 			y->right_->parent_ = cur_node;
@@ -244,8 +237,8 @@ private:
 	 *      [b]  [g]       [a]  [b]
 	 *  x = cur_node
 	*/
-	void left_rotate(Node<KeyT> *cur_node) {
-		Node<KeyT> *y = cur_node->right_;
+	void left_rotate(std::shared_ptr<Node<KeyT>> cur_node) {
+    std::shared_ptr<Node<KeyT>> y = cur_node->right_;
 		cur_node->right_ = y->left_;
 		if (y->left_ != nullptr)
 			y->left_->parent_ = cur_node;
